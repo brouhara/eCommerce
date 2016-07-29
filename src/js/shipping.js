6b7c4c64ea2d2a272ed2638e910920bf02acabc8
brouhara <adam.brouhard@gmail.com>
2016-07-29 14:27:22 -0600

    Removing manually entered shipping fields from html, added javascript to build form sections dynamically.

--- src/js/shipping.js
+++ src/js/shipping.js
@@ -4,31 +4,82 @@
 //      <input type="text" class="form-control" id="firstName" name="firstName">
 // </div>
 
+var States = ['Alaska', 'Colorado', 'Delaware', 'Iowa', 'Nebraska'];
 
-var Shipping = {
-	firstName: { },
-	lastName: { },
-	company: { },
-	address1: { },
-	address2: { },
-	city: { },
-	state: { },
-	zip: { }
+function Field(type, name, text, span) {
+	this.type = type;
+	this.name = name;
+	this.text = text;
+	this.span = span;
 }
 
-function formAppend() {
-	var $form = $('.row#shipping');
+var Shipping = [
 
-	var firstName = formInput(textField('firstName', 'firstName', 'First Name'));
-	// var lastName = formInput( 'text', 'form-control', 'firstName', 'firstName', 'First Name');
-	// var company = formInput( 'text', 'form-control', 'firstName', 'firstName', 'First Name');
-	// var firstName = formInput( 'text', 'form-control', 'firstName', 'firstName', 'First Name');
-	// var firstName = formInput( 'text', 'form-control', 'firstName', 'firstName', 'First Name');
-	console.log(firstName);
-	$form.append(firstName);
+	[
+		{ firstName: ['input', 'First Name', 'col-md-4'] },
+		{ lastName: ['input', 'Last Name', 'col-md-4'] },
+		{ company: ['input', 'Company Name', 'col-md-4'] },
+	],
+
+	[
+		{ address1: ['input', 'Address Line 1', 'col-md-6'] },
+		{ address2: ['input', 'Address Line 2', 'col-md-6'] },
+	],
+
+	[
+		{ city: ['input', 'City', 'col-md-4'] },
+		{ state: ['select', 'State', 'col-md-4']},
+		{ zip: ['input', 'Zip Code', 'col-md-4']}
+	]
+]
+
+
+function buildForm() {
+	var $Shipping = $('div.row#shipping');
+
+	for( rowNum in Shipping) {
+		var $row = buildRow( $Shipping, Shipping[rowNum]);
+		$form.append($row);
+	}
+
+	return $form;
 }
 
-function textField ( fieldName, fieldText, fieldSpan ) {
+function buildRow ($parent, row) {
+	var $row = createRow();
+	var fields = Object.keys(row);
+
+	fields.forEach(function(fieldNum) {
+		var $field = row[fieldNum];
+		$parent.append($parent, $field);
+	});
+}
+
+function buildField ( $parent, item ) {
+	
+	var $field = formInput(textField(newField));
+
+}
+
+function createRow() {
+	var $row = $(document.createElement('div'))
+		.addClass('row');
+	return $row;
+}
+
+function optionField({fieldName, fieldOptions, fieldSpan}) {
+	var field = {
+		classes: 'form-control',
+		id: fieldName,
+		name: fieldName,
+		options: fieldOptions,
+		span: fieldSpan
+	}
+
+	return field;
+}
+
+function textField({fieldName, fieldText, fieldSpan}) {
 	var field = {
 		type: 'text',
 		classes: 'form-control',
@@ -41,43 +92,57 @@ function textField ( fieldName, fieldText, fieldSpan ) {
 	return field;
 }
 
-function formInput( {type, classes, id, name, text, span} ) {
-    var $div = fieldDiv( span );
+function formInput({ classes, id, name, span, text, type }) {
+	var $div = fieldDiv(span);
 
-    var $label = fieldLabel(name, text);
-    var $input = fieldInput( type, classes, id, name);
+	var $label = fieldLabel(name, text);
+	var $input = fieldInput(type, classes, span, id, name);
 
-    $div.append($label);
-    $label.append($input);
+	$div.append($label);
+	$label.append($input);
 
-    return $div;
-}
-
-function fieldDiv( fieldSpan ) {
-	var $div = $( document.createElement('div') )
-					.addClass('form-group ' + fieldSpan );
 	return $div;
 }
 
-function fieldLabel( fieldName, labelText) {
+function fieldDiv(fieldSpan) {
+	var $div = $(document.createElement('div'))
+		.addClass('form-group ' + fieldSpan);
+	return $div;
+}
 
-	var $label = $( document.createElement('label') )
-					
+function fieldLabel(fieldName, labelText) {
+
+	var $label = $(document.createElement('label'));
+
 	$label.text(labelText);
-	
-    return $label;
+
+	return $label;
 }
 
-function fieldInput(fieldType, fieldClass, fieldId, fieldName) {
+function fieldInput(fieldType, fieldClass, fieldSpan, fieldId, fieldName) {
 	var $input = $(document.createElement('input'))
-					.addClass(fieldClass)
-					.attr( "type", fieldType)
-					.attr( "id", fieldId)
-					.attr( "name", fieldName);
+		.addClass(fieldClass + ' ' + fieldSpan)
+		.attr("type", fieldType)
+		.attr("id", fieldId)
+		.attr("name", fieldName);
 
-    return $input;
+	return $input;
 }
 
+function fieldOption(fieldClass, fieldSpan, fieldId, fieldName, fieldOptions) {
+	var $select = $(document.createElement('select'))
+		.addClass(fieldClass + ' ' + fieldSpan)
+		.attr("id", fieldId)
+		.attr("name", fieldName);
 
-formAppend();
+	for (option in fieldOptions) {
+		var $option = $(document.createElement('option'))
+			.text(option);
+		$select.append($option);
+	}
+
+	return $select;
+}
+
+var $form = buildForm();
 
